c1800e66c498e77f96df95427fa58f0b2164ecd1
Khalid <shkhalid95@mail.ru>
2023-02-05 05:15:52 +0300

    refactor: doc

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,4 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 4000,
+  baseURL: process.env.BASE_URL || 'http://localhost:4000',
 });
--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,17 @@ import configuration from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const envConfig = configuration();
 
-  const config = new DocumentBuilder()
-    .setTitle('REST Service')
-    .setDescription('The REST Service API description')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  // const config = new DocumentBuilder()
+  //   .setTitle('REST Service')
+  //   .setDescription('The REST Service API description')
+  //   .setVersion('1.0')
+  //   .addServer(envConfig.baseURL)
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(configuration().port);
+  await app.listen(envConfig.port);
 }
 bootstrap();
